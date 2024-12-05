85abd56d7a5b553d009495c6cbd2fdd95863d9cd
Burnett Miao <searchfor1987@gmail.com>
2024-12-05 18:01:11 +0800

    資料夾檔案調整

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('button');
-  btn.addEventListener('click', clickBtn);
-
-  function clickBtn() {
-    console.log('Button clicked!!!!');
-  }
-});
